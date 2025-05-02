c3598c0cc235d41d97629f23aabc084454008874
urjellyfish <annaphan0511@gmail.com>
2025-05-02 23:37:07 +0700

    feature : add ProductSupplier entity

---
+++ Domain/Entities/ProductSupplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class ProductSupplier
+    {
+        public int ProductId { get; set; }
+        public int SupplierId { get; set; }
+    }
+}
