687e1e23b1a2973a836aff8fba9118c709e81114
urjellyfish <annaphan0511@gmail.com>
2025-05-02 20:55:28 +0700

    feat: add Supplier entity

---
+++ Domain/Entities/Suppliers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Suppliers
+    {
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public int PrioritizeScore { get; set; }
+        public string Status { get; set; }
+        public ICollection<ProductSupplier> productSuppliers { get; set; }
+    }
+}
