950d1f3d49034b2f61b11e4d8ff2ee21bf31ce3f
urjellyfish <annaphan0511@gmail.com>
2025-05-03 21:32:07 +0700

    feat : add Inventory entity

---
+++ Domain/Entities/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Inventory
+    {
+        public int InventoryId { get; set; }
+        public DateOnly ExpiryDate { get; set; }
+        public int CurrentQuantity { get; set; }
+        public int ProductId { get; set; }
+    }
+}
