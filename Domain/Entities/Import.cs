4fdb12829d9835e728eb0b568025c929fcf37012
urjellyfish <annaphan0511@gmail.com>
2025-05-03 21:28:24 +0700

    feat : add Import entity

---
+++ Domain/Entities/Import.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Import
+    {
+        public int ImportId { get; set; }
+        public decimal TotalPayment { get; set; }
+        public string Status { get; set; }
+        public int SupplierId { get; set; }
+        public ICollection<ImportItem> ImportItems { get; set; }
+    }
+}
