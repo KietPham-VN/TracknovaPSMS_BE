1ba4af4bc31731ac9dbc92e311acb07b721b4bd1
KIETTA-TAK <trananhkiet21082005@gmail.com>
2025-05-03 11:11:16 +0700

    feat: create Pricing entity

---
+++ Domain/Entities/Pricing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Pricing
+    {
+        public int PricingId { get; set; }
+        public int ProductId { get; set; }
+        public decimal PricingValue { get; set; }
+        public string UnitType { get; set; }
+        public int ExchangeQuantity { get; set; }
+    }
+}
