c46ffbe53d46a44b31a34be03b748ebce0cf52a4
urjellyfish <annaphan0511@gmail.com>
2025-05-03 22:56:13 +0700

    fix : add enum for Supplier

--- Domain/Entities/Suppliers.cs
+++ Domain/Entities/Suppliers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ namespace Domain.Entities
         public int SupplierId { get; set; }
         public string SupplierName { get; set; }
         public int PrioritizeScore { get; set; }
-        public string Status { get; set; }
+        public SupplierStatusEnum Status { get; set; }
         public ICollection<ProductSupplier> productSuppliers { get; set; }
     }
 }
