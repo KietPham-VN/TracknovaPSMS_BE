00bd995cc47837c3e7a802237d9f6f9143d8ace0
urjellyfish <annaphan0511@gmail.com>
2025-05-03 12:05:19 +0700

    feat : add Category entity

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int PrioritizeScore { get; set; }
+        public string Status { get; set; }
+        public ICollection<ProductCategory> ProductCategories { get; set; }
+    }
+}
